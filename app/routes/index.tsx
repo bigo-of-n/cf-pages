decbc0adf144bfdb286eddd10f136121d195de02
Chandan Kumar <chandankumar.r@gmail.com>
2022-05-12 15:36:37 +0530

    Update index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Chandan Kumar on Cloud(Flare)</h1>
       <p>Cloudflare | Chandankumar.com</p>
     </div>
   );
